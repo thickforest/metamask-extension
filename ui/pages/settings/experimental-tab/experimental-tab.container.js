400832fccfdaddf4063204f3a85a1ccf440d79d8
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2021-12-14 13:54:09 -0500

    lint fix

--- ui/pages/settings/experimental-tab/experimental-tab.container.js
+++ ui/pages/settings/experimental-tab/experimental-tab.container.js
@@ -20,7 +20,7 @@ const mapStateToProps = (state) => {
     useTokenDetection: getUseTokenDetection(state),
     useCollectibleDetection: getUseCollectibleDetection(state),
     openSeaEnabled: getOpenSeaEnabled(state),
-    theme: getTheme(state)
+    theme: getTheme(state),
   };
 };
 
